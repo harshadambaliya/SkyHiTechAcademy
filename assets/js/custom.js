c73fb1a7d883a25a172ee989e87497aa7c374b59
harshadambaliya <harshambaliya.notioninfosoft@gmail.com>
2021-12-30 19:01:33 +0530

    working

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    $('.multiple-items').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
+    // $('.multiple-items').slick({
+    //     infinite: true,
+    //     slidesToShow: 3,
+    //     slidesToScroll: 3
+    // });
+    $(".shta-social-share-btn").on('click', function() {
+        $(this).closest('.shta-look-great-card-body').children().find(".shta-share-media-list").addClass('show');
     });
 });
